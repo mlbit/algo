90c7d0b83b34a7521a4b1c9c384a43ab3dae2eb7
Mervyn Badiang <mlbit@users.noreply.github.com>
2021-01-24 19:43:15 -0500

    Add logic for bidirectional link (#4)
    * Update test
    
    * Clean gradle & gitignore
    
    * Update license
    
    * Add gitignore
    
    * Update test
    
    * Remove .graddle dir
    
    * Update stack logic to support bidirection iteration.
    
    * Faile build if any checkstyle warning & errors.

--- src/main/java/com/weldbit/algo/stack/Stack.java
+++ src/main/java/com/weldbit/algo/stack/Stack.java
@@ -1,19 +1,19 @@
 /**
  * MIT License
  *
- * Copyright (c) 2021 Mervyn Badiang
+ * <p>Copyright (c) 2021 Mervyn Badiang
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * <p>Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in all
+ * <p>The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * <p>THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
@@ -24,45 +24,156 @@
 
 package com.weldbit.algo.stack;
 
+/**
+ * A generic Stack class that allow multiple ways to traverse records.
+ *
+ */
 public class Stack<T> {
 
     private Node<T> topNode = null;
-    private Integer size = 0;
     private Node<T> bottomNode = null;
+    private Node<T> pointerNode;
+    private Integer size = 0;
+    private Integer indexPosition = -1;
 
     private class Node<J> {
-        J item;
-        Node<J> prevNode;
+        private J item;
+        private Node<J> prevNode;
+        private Node<J> nextNode;
+
+        public void setItem(J item) {
+            this.item = item;
+        }
+
+        public J getItem() {
+            return item;
+        }
+
+        public void setPrevNode(Node<J> prevNode) {
+            this.prevNode = prevNode;
+        }
+
+        public Node<J> getPrevNode() {
+            return this.prevNode;
+        }
+
+        public void setNextNode(Node<J> nextNode) {
+            this.nextNode = nextNode;
+        }
+
+        public Node<J> getNextNode() {
+            return this.nextNode;
+        }
     }
 
+    private void setTopNode(Node<T> node) {
+        topNode = node;
+    }
+
+    private Node<T> getTopNode() {
+        return topNode;
+    }
+
+    private void setBottomNode(Node<T> node) {
+        bottomNode = node;
+    }
+
+    private Node<T> getBottomNode() {
+        return bottomNode;
+    }
+
+    private void setPointerNode(Node<T> node) {
+        this.pointerNode = node;
+    }
+
+    private Node<T> getPointerNode() {
+        return this.pointerNode;
+    }
+
+    /**
+     * Add new record to the top of the stack.
+     *
+     * @param item - add new item
+     */
     public void push(T item) {
-        Node<T> orgTopNode = topNode;
+        Node<T> orgTopNode = getTopNode();
+
         topNode = new Node<>();
-        topNode.item = item;
-        topNode.prevNode = orgTopNode;
-        if (orgTopNode == null)
-            bottomNode = topNode;
-        size++;
+        topNode.setItem(item);
+        topNode.setPrevNode(orgTopNode);
+
+        if (orgTopNode == null) {
+            setBottomNode(topNode);
+            setPointerNode(getBottomNode());
+        } else {
+            orgTopNode.setNextNode(topNode);
+        }
+
+        setSize(getSize() + 1);
+
     }
 
+    /**
+     * Remove the top record in the stack.
+     *
+     * @return value of the top record.
+     */
     public T pop() {
-        if (topNode == null)
+        if (topNode == null) {
             return null;
-        T item = topNode.item;
-        topNode = topNode.prevNode;
-        size--;
+        }
+        T item = topNode.getItem();
+        setTopNode(topNode.getPrevNode());
+        setSize(getSize() - 1);
         return item;
     }
 
     public T getTop() {
-        return topNode.item;
+        return topNode.getItem();
     }
 
     public T getBottom() {
-        return bottomNode.item;
+        return getBottomNode().getItem();
     }
 
-    public Integer size() {
+    private void setSize(Integer lsize) {
+        size = lsize;
+    }
+
+    public Integer getSize() {
         return size;
+    }
+
+    private void setIndexPosition(Integer index) {
+        indexPosition = index;
+    }
+
+    public void resetRecordPosition() {
+        setIndexPosition(0);
+        setPointerNode(getBottomNode());
+    }
+
+    public Integer getIndexPosition() {
+        return this.indexPosition;
+    }
+
+    public T iteratorValue() {
+        return this.getPointerNode().getItem();
+    }
+
+    /**
+     * Move node pointer to next available and return value.
+     *
+     * @return - item value.
+     */
+    public T iteratorNext() {
+        if (this.getPointerNode().getNextNode() == null) {
+            return null;
+        }
+
+        this.setPointerNode(this.getPointerNode().getNextNode());
+        indexPosition++;
+
+        return this.getPointerNode().getItem();
     }
 }
--- src/test/java/com/weldbit/algo/stack/StackTest.java
+++ src/test/java/com/weldbit/algo/stack/StackTest.java
@@ -7,11 +7,22 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+/**
+ * Test Stack function with 100 coverage.
+ */
 public class StackTest {
-    Stack<Integer> stackint;
+    transient Stack<Integer> stackint;
 
+    /**
+     * Set initial values that will be use to verify logic.
+     */
     public StackTest() {
         stackint = new Stack<Integer>();
+        stackint.push(10);
+        stackint.push(20);
+        stackint.push(30);
+        stackint.push(40);
+        stackint.push(50);
     }
 
     @BeforeAll
@@ -19,6 +30,12 @@ public class StackTest {
         System.out.println("@BeforeAll executed");
     }
 
+    /**
+     * Test push logic using parameterized values.
+     *
+     * @param item - parameterized values.
+     *
+     */
     @ParameterizedTest
     @ValueSource(ints = { 1, 3, 7, 5, Integer.MAX_VALUE })
     @DisplayName("Test stack push.")
@@ -30,12 +47,56 @@ public class StackTest {
 
     @Test
     public void testPop() {
-        System.out.println("Test pop integer value:" + stackint.size());
-        while (stackint.size() > 0) {
+        System.out.println("Test pop integer value:" + stackint.getSize());
+        while (stackint.getSize() > 0) {
             Integer topval = stackint.getTop();
             Integer popval = stackint.pop();
             Assertions.assertEquals(topval, popval);
         }
+
+        // Test if everything were removed
+        Integer popval = stackint.pop();
+        Assertions.assertNull(popval);
     }
-    
+
+    @Test
+    public void testTop() {
+        Integer topval = stackint.getTop();
+        Assertions.assertEquals(50, topval);
+    }
+
+    @Test
+    public void testBottom() {
+        Integer bottomval = stackint.getBottom();
+        Assertions.assertEquals(10, bottomval);
+    }
+
+    @Test
+    public void testPosition() {
+        stackint.resetRecordPosition();
+        Integer position = stackint.getIndexPosition();
+        Assertions.assertEquals(0, position);
+    }
+
+    @Test
+    public void testIteratorPositionValue() {
+        Integer position = stackint.getIndexPosition();
+        Assertions.assertEquals(-1, position);
+        stackint.resetRecordPosition();
+        position = stackint.getIndexPosition();
+        Assertions.assertEquals(0, position);
+        Integer actual = stackint.iteratorValue();
+        Assertions.assertEquals(10, actual);
+        actual = stackint.iteratorNext();
+        Assertions.assertEquals(20, actual);
+        actual = stackint.iteratorNext();
+        Assertions.assertEquals(30, actual);
+        actual = stackint.iteratorNext();
+        Assertions.assertEquals(40, actual);
+        actual = stackint.iteratorNext();
+        Assertions.assertEquals(50, actual);
+        actual = stackint.iteratorNext();
+        Assertions.assertEquals(null, actual);
+    }
+
 }
